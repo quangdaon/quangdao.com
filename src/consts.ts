c183019ae6555a39ca25bd8693eb33e80af9338d
Quangdao Nguyen <me@quangdao.com>
2023-03-21 08:20:22 -0500

    Layout Stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Quangdao Nguyen';
+export const SITE_DESCRIPTION = 'Hi! 👋 My name is Quangdao (most call me Q) and I am a well-versed, multi-talented, highly-functional robot.';
