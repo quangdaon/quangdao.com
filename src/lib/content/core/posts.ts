22ca5eed6bffeeecd017e4a5871e8fac4b7a114d
Quangdao Nguyen <me@quangdao.com>
2023-03-28 21:22:59 -0500

    compile posts

--- src/lib/content/core/posts.ts
+++ src/lib/content/core/posts.ts
@@ -1,35 +1,44 @@
 import type { PostType, PostData } from './definition-types';
 import { definitions } from '../definitions';
 
-let postsCache: { [K in PostType]?: PostData<K>[] } = {};
+type PostsCache = {
+	[K in PostType]?: PostData<K>[];
+};
 
-export async function loadPosts<T extends PostType>(postType: T): Promise<PostData<T>[]> {
-	const postsRoot = `/src/content/${postType}`;
+let postsCache: PostsCache;
+
+async function compilePosts() {
 	const allPostFiles = import.meta.glob(`/src/content/**/*.md`);
 	const iterablePostFiles = Object.entries(allPostFiles);
-	const relevantPostFiles = iterablePostFiles.filter(([path]) => path.startsWith(postsRoot));
 
-	if (postType in postsCache) return postsCache[postType] as PostData<T>[];
+	postsCache = {};
 
-	const posts = await Promise.all<PostData<T>>(
-		relevantPostFiles.map(async ([path, resolver]) => {
-			const result = (await resolver()) as any;
-			const { metadata, default: component } = result;
+	let postType: PostType;
+	for (postType in definitions) {
+		const postsRoot = `/src/content/${postType}`;
+		const relevantPostFiles = iterablePostFiles.filter(([path]) => path.startsWith(postsRoot));
+		const posts = await Promise.all<PostData<typeof postType>>(
+			relevantPostFiles.map(async ([path, resolver]) => {
+				const result = (await resolver()) as any;
+				const { metadata, default: component } = result;
+	
+				const data = {
+					...metadata,
+					slug: metadata.slug || path.replace(new RegExp(`^${postsRoot}/(.*?).md$`), '$1'),
+					component
+				};
+	
+				return definitions[postType].transform(data);
+			})
+		);
+	
+		postsCache[postType] = posts as any;
+	}
+}
 
-			const data = {
-				...metadata,
-				slug: metadata.slug || path.replace(new RegExp(`^${postsRoot}/(.*?).md$`), '$1'),
-				component
-			};
-
-			const transformed = definitions[postType].transform(data);
-			return transformed;
-		})
-	);
-
-	postsCache[postType] = posts as any;
-
-	return posts;
+export async function loadPosts<T extends PostType>(postType: T): Promise<PostData<T>[]> {
+	if (!postsCache) await compilePosts();
+	return postsCache[postType] || [];
 }
 
 export async function loadPost<T extends PostType>(
--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,6 +1,6 @@
 import { loadPosts } from '$lib/content';
 
 export async function load() {
-  const posts = await loadPosts('blog');
+	const posts = (await loadPosts('blog')).sort((a, b) => (a.date = b.date));
 	return { posts };
 }
