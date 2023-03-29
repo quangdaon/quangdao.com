0b0c194343792da33acb28a93d28cd8215a6e1dc
Quangdao Nguyen <qnguyen@nvisia.com>
2023-03-29 08:14:57 -0500

    Move splash and add head content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../styles/main.scss';
 </script>
 
+<svelte:head>
+	<title>Quangdao Nguyen</title>
+	<meta name="description" content="Quangdao is a creative, adaptable, and resourceful software engineer with a passion for pushing limits and challenging impossiblities.">
+</svelte:head>
+
 <div class="app">
 		<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <script>
+	import SplashScreen from '$lib/components/splash/SplashScreen.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		Hello World
-	</h1>
-</section>
+<SplashScreen />
