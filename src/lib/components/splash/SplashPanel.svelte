bdc63581450f9db172fd01ab8903169cb94ce28c
Quangdao Nguyen <qnguyen@nvisia.com>
2023-03-29 09:40:12 -0500

    Initial Splash Panels

---
+++ src/lib/components/splash/SplashPanel.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let label: string;
+	const hue = Math.floor(Math.random() * 361);
+</script>
+
+<div class="panel" style="--hue: {hue}">
+	<div class="content">
+		<h2>{label}</h2>
+	</div>
+</div>
+
+<style lang="scss">
+	.panel {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex: 1 1 auto;
+		background: hsl(var(--hue), 70%, 80%); // temp
+		$background-colors: (pink, lightgreen, lightblue, periwinkle);
+		@for $i from 1 through length($background-colors) {
+			$color: nth($background-colors, $i);
+			&:nth-child(#{$i}) {
+				background-color: $color;
+			}
+		}
+	}
+</style>
--- src/lib/components/splash/SplashScreen.svelte
+++ src/lib/components/splash/SplashScreen.svelte
@@ -1,5 +1,28 @@
-<section>
-	<h1>
-		Hello World
-	</h1>
-</section>
+<script>
+	import SplashPanel from './SplashPanel.svelte';
+</script>
+
+<div class="splash">
+  <!-- well-versed -->
+  <SplashPanel label="Blog" />
+
+  <!-- multi-talented -->
+  <SplashPanel label="Uses" />
+  
+  <!-- highly-functional -->
+  <SplashPanel label="Projects" />
+  
+  <!-- robot -->
+  <SplashPanel label="Bio" />
+</div>
+
+<style lang="scss">
+  @use '/src/styles/utils/mixins';
+  .splash {
+    display: flex;
+    justify-content: stretch;
+    flex-direction: column;
+    position: absolute;
+    @include mixins.fill-container;
+  }
+</style>
