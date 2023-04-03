37ab34d54f51a7b28c2caeb80e2bcb298aab517e
Quangdao Nguyen <me@quangdao.com>
2023-04-02 23:26:21 -0500

    Footer

--- src/lib/components/layout/SiteFooter.svelte
+++ src/lib/components/layout/SiteFooter.svelte
@@ -1,12 +1,47 @@
-<footer>&copy; Quangdao</footer>
+<script>
+	import SocialIcons from '../shared/SocialIcons.svelte';
+</script>
+
+<footer>
+	<div class="footer-content">
+		<div>
+			<p>&copy; Quangdao</p>
+		</div>
+		<div class="social">
+			<SocialIcons />
+		</div>
+	</div>
+</footer>
 
 <style lang="scss">
 	@use '~/breakpoints';
 	@use '~/placeholders';
 
 	footer {
+		position: relative;
+		z-index: 2;
+		margin-top: 2rem;
+	}
+
+	.footer-content {
 		@extend %container;
-    margin-top: 2rem;
-    padding: 1px;
+		padding: 1em;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		p {
+			margin: 0;
+		}
+	}
+
+	.social {
+		display: flex;
+		:global(a:hover) {
+			--color-foreground: var(--color-purple-base);
+		}
+		:global(svg) {
+			width: 1em;
+			padding: 1em;
+		}
 	}
 </style>
