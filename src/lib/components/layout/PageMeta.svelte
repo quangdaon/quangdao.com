903842ca6f73e5d7aee357016570ff938371cffb
Quangdao Nguyen <me@quangdao.com>
2023-04-03 21:40:37 -0500

    Social image

--- src/lib/components/layout/PageMeta.svelte
+++ src/lib/components/layout/PageMeta.svelte
@@ -7,12 +7,14 @@
 	<meta name="description" content={$page.data.pageDescription} />
 
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content={$page.url} />
+	<meta property="og:url" content={$page.url.toString()} />
 	<meta property="og:title" content={$page.data.pageTitle} />
 	<meta property="og:description" content={$page.data.pageDescription} />
-
+	<meta property="og:image" content="/images/social.png" />
+	
 	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content={$page.url} />
+	<meta property="twitter:url" content={$page.url.toString()} />
 	<meta property="twitter:title" content={$page.data.pageTitle} />
 	<meta property="twitter:description" content={$page.data.pageDescription} />
+	<meta property="twitter:image" content="/images/social.png" />
 </svelte:head>
