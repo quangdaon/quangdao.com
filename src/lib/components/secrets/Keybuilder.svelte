5388e7edb657d6aea8baf760b3a341ff6411930f
Quangdao Nguyen <me@quangdao.com>
2023-04-04 08:00:33 -0500

    Start keyhole

---
+++ src/lib/components/secrets/Keybuilder.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Key from './Key.svelte';
+	const teeth = [0, 0, 0, 0, 0, 0];
+	$: key = teeth.join('');
+</script>
+
+<div class="keybuilder">
+	<div class="keysizer">
+		{#each teeth as tooth, i}
+			<input type="number" bind:value={teeth[i]} max="5" min="0" />
+		{/each}
+	</div>
+	<div class="key">
+		<Key {key} />
+	</div>
+</div>
+
+<style>
+	.keybuilder {
+		position: fixed;
+		width: 200px;
+		left: 0;
+		top: 0;
+    z-index: 20;
+	}
+
+  .keysizer {
+    display: flex;
+  }
+
+	.key {
+		width: 100%;
+	}
+</style>
--- src/lib/components/secrets/Keyhole.svelte
+++ src/lib/components/secrets/Keyhole.svelte
@@ -1,9 +1,16 @@
+<script>
+	import Keybuilder from './Keybuilder.svelte';
+
+</script>
+
 <button class="keyhole" title="Hmm... What could this possibly be?">
 	<svg viewBox="0 0 100 150">
 		<circle cx="50" cy="50" r="50" class="keyhole-shape" />
 		<rect x="25" y="60" rx="10" width="50" height="75" class="keyhole-shape" />
 	</svg>
 </button>
+
+<Keybuilder />
 
 <style>
 	svg {
