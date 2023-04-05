4c111f13f58b654267938682e0fb1acdfcfae7c9
Quangdao Nguyen <qnguyen@nvisia.com>
2023-04-05 10:48:17 -0500

    Animate name switch

--- src/lib/components/layout/FooterName.svelte
+++ src/lib/components/layout/FooterName.svelte
@@ -1,31 +1,41 @@
 <script>
 	import { tooltip } from '$lib/actions/tooltip';
+	import { fly } from 'svelte/transition';
 	let useVietnameseName = false;
 </script>
 
-<div class="name" on:dblclick={() => (useVietnameseName = !useVietnameseName)}>
-  {#if useVietnameseName}
-    <p lang="vn">Nguyễn Quang Đạo</p>
-    <p class="pronunciation">
-      (<abbr use:tooltip={'"ng" is like the suffix -ing; upward pitch like you are asking a question'}
-        >ngween</abbr
-      >
-      <abbr
-        use:tooltip={'Exactly how someone trying to impersonate a stereotypical eastern accent would say "one"; neutral tone'}
-        >wahng</abbr
-      >
-      <abbr use:tooltip={'same as the anglocized "dow (Jones)" except with a heavy tone'}>dow</abbr>)
-    </p>
-  {:else}
-    <p>Quangdao Nguyen</p>
-    <p class="pronunciation">
-      (<abbr use:tooltip={'Rhymes with "Ron"'}>kwahn</abbr>&bull;<abbr use:tooltip={'As in "Dow Jones"'}
-        >dow</abbr
-      >
-      <abbr use:tooltip={'Like "win" but with another N in front'}>nwin</abbr>)
-    </p>
-  {/if}
-</div>
+{#key useVietnameseName}
+	<div
+		class="name"
+		on:dblclick={() => (useVietnameseName = !useVietnameseName)}
+		in:fly={{ duration: 200, y: 20 }}
+	>
+		{#if useVietnameseName}
+			<p lang="vn">Nguyễn Quang Đạo</p>
+			<p class="pronunciation">
+				(<abbr
+					use:tooltip={'"ng" is like the suffix -ing; upward pitch like you are asking a question'}
+					>ngween</abbr
+				>
+				<abbr
+					use:tooltip={'Exactly how someone trying to impersonate a stereotypical eastern accent would say "one"; neutral tone'}
+					>wahng</abbr
+				>
+				<abbr use:tooltip={'same as the anglocized "dow (Jones)" except with a heavy tone'}
+					>dow</abbr
+				>)
+			</p>
+		{:else}
+			<p>Quangdao Nguyen</p>
+			<p class="pronunciation">
+				(<abbr use:tooltip={'Rhymes with "Ron"'}>kwahn</abbr>&bull;<abbr
+					use:tooltip={'As in "Dow Jones"'}>dow</abbr
+				>
+				<abbr use:tooltip={'Like "win" but with another N in front'}>nwin</abbr>)
+			</p>
+		{/if}
+	</div>
+{/key}
 
 <style lang="scss">
 	.name {
