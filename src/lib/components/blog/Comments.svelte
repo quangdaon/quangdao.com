cff86c9769e3627812011b200ffe5618201d0c8a
Quangdao Nguyen <me@quangdao.com>
2023-04-09 11:33:27 -0500

    Comments via Utteranc.es

---
+++ src/lib/components/blog/Comments.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+  
+	const options: Record<string, string> = {
+		src: 'https://utteranc.es/client.js',
+		repo: 'quangdaon/quangdao.com',
+		label: 'comments',
+		crossorigin: 'anonymous',
+		theme: 'preferred-color-scheme',
+		async: '',
+		'issue-term': 'pathname'
+	};
+
+	onMount(() => {
+		const utteranceScript = document.createElement('script');
+		const targetTag = document.getElementById('utterances-comments');
+
+		for (const prop in options) {
+			utteranceScript.setAttribute(prop, options[prop]);
+		}
+
+    targetTag?.appendChild(utteranceScript);
+	});
+</script>
+
+<noscript>JavaScript must be enabled to leave comments.</noscript>
+<div id="utterances-comments" />
