3bbf465101f669f1ec6f85c37533a65654cac807
Quangdao Nguyen <me@quangdao.com>
2023-04-09 11:49:20 -0500

    Timeout to test

--- src/lib/components/blog/Comments.svelte
+++ src/lib/components/blog/Comments.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-  
+
 	const options: Record<string, string> = {
 		src: 'https://utteranc.es/client.js',
 		repo: 'quangdaon/quangdao.com',
@@ -12,14 +12,16 @@
 	};
 
 	onMount(() => {
-		const utteranceScript = document.createElement('script');
-		const targetTag = document.getElementById('utterances-comments');
-
-		for (const prop in options) {
-			utteranceScript.setAttribute(prop, options[prop]);
-		}
-
-    targetTag?.appendChild(utteranceScript);
+    setTimeout(() => {
+      const utteranceScript = document.createElement('script');
+      const targetTag = document.getElementById('utterances-comments');
+  
+      for (const prop in options) {
+        utteranceScript.setAttribute(prop, options[prop]);
+      }
+  
+      targetTag?.appendChild(utteranceScript);
+    }, 500);
 	});
 </script>
 
