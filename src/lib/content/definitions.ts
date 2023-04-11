41266c1d9173f95407671a72e9803bf0183c3586
Quangdao Nguyen <me@quangdao.com>
2023-04-10 23:49:33 -0500

    Filter out future posts

--- src/lib/content/definitions.ts
+++ src/lib/content/definitions.ts
@@ -1,13 +1,21 @@
+import { dev } from '$app/environment';
 import { definePostType } from './core/definition-helpers';
 import type { BlogPost, ComicPost, ProjectPost } from './types';
 
 export const definitions = {
 	blog: definePostType<BlogPost>({
-		slug: (str: string) => str.replace(/^\d{4}-\d{2}-\d{2}-/, ''), // Strips out date
-		date: (str) => new Date(str)
+		transformations: {
+			slug: (str: string) => str.replace(/^\d{4}-\d{2}-\d{2}-/, ''), // Strips out date
+			date: (str) => new Date(str)
+		},
+		filter({ date }) {
+			return dev || date < new Date()
+		} 
 	}),
 	comics: definePostType<ComicPost>({
-		alt: (alt, obj) => alt || obj.title
+		transformations: {
+			alt: (alt, obj) => alt || obj.title
+		}
 	}),
 	projects: definePostType<ProjectPost>({})
 };
