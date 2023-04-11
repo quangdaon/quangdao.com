668e01e7b01946358ede80ef7f2d8a3d0636ffdc
Quangdao Nguyen <me@quangdao.com>
2023-04-11 00:40:23 -0500

    Fix page meta

--- src/routes/(website)/blog/[slug]/+page.ts
+++ src/routes/(website)/blog/[slug]/+page.ts
@@ -2,13 +2,16 @@ import type { PageLoadEvent } from './$types';
 import { getPost } from '$lib/content';
 import { error } from '@sveltejs/kit';
 
-export async function load({ params, data }: PageLoadEvent) {
+export async function load({ params }: PageLoadEvent) {
 	const post = await getPost('blog', params.slug);
 	if (!post) throw error(404);
-	return {
+	let data: Record<string, any> = {
 		post,
 		pageTitle: `${post.title} | Quangdao's Blog`,
-		pageDescription: post.description,
-		pageSocialImage: post.socialImage ?? (data as any).pageSocialImage
+		pageDescription: post.description
 	};
+
+	if (post.socialImage) data.pageSocialImage = post.socialImage;
+	
+	return data;
 }
