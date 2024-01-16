dc248dc15da527aaa05fd012c5db9754b7aa8adf
Quangdao Nguyen <qnguyen@nvisia.com>
2024-01-16 09:18:56 -0600

    Breakdown

--- src/lib/components/diagrams/binary/BaseConverter.svelte
+++ src/lib/components/diagrams/binary/BaseConverter.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { getBaseName } from '$lib/config/numeric-bases';
 	import BaseNumberInput from './BaseNumberInput.svelte';
+	import BaseConverterBreakdown from './BaseConverterBreakdown.svelte';
 
 	export let baseIn: number;
 	export let baseOut: number;
+	let breakdownOpen = false;
 
 	let valueIn = '0';
 	let valueOut = '0';
@@ -28,23 +30,35 @@
 	};
 </script>
 
-<div class="converter">
-	<div>
-		<h3>{getBaseName(baseIn)}</h3>
-		<BaseNumberInput base={baseIn} bind:value={valueIn} on:input={handleInputChange} />
+<div class="wrapper">
+	<div class="converter">
+		<div>
+			<h3>{getBaseName(baseIn)}</h3>
+			<BaseNumberInput base={baseIn} bind:value={valueIn} on:input={handleInputChange} />
+		</div>
+
+		<div>
+			<h3>{getBaseName(baseOut)}</h3>
+			<BaseNumberInput base={baseOut} bind:value={valueOut} on:input={handleOutputChange} />
+		</div>
 	</div>
 
-	<div>
-		<h3>{getBaseName(baseOut)}</h3>
-		<BaseNumberInput base={baseOut} bind:value={valueOut} on:input={handleOutputChange} />
-	</div>
+	{#if parseInt(valueIn, baseIn) > 0}
+		<details bind:open={breakdownOpen}>
+			<summary>Show Breakdown</summary>
+			<BaseConverterBreakdown {baseIn} {baseOut} value={valueIn} />
+		</details>
+	{/if}
 </div>
 
 <style lang="scss">
 	@use '~/breakpoints';
 
-	.converter {
+	.wrapper {
 		margin: var(--spacing) 0;
+	}
+
+	.converter {
 		text-align: center;
 		div {
 			margin: var(--spacing) 0;
