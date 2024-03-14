82fc2635d72217bd5dc069d3397e25bc64c2c667
Quangdao Nguyen <qnguyen@nvisia.com>
2024-03-14 00:00:07 -0500

    Pi Day

---
+++ src/lib/components/diagrams/SvgDiagram.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let viewBox: string;
+</script>
+
+<div class="diagram">
+	<svg {viewBox} {...$$props}>
+		<slot />
+	</svg>
+</div>
+
+
+<style>
+  svg :global(.stroke), svg :global(line) {
+    fill: transparent;
+    stroke: var(--color-foreground);
+  }
+</style>
