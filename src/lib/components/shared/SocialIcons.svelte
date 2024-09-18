be85b906d5ce6e5ecc48d58543995920c236ac0b
Quangdao Nguyen <qnguyen@nvisia.com>
2024-09-18 14:04:10 -0500

    Switch SVG icons for iconify

--- src/lib/components/shared/SocialIcons.svelte
+++ src/lib/components/shared/SocialIcons.svelte
@@ -1,21 +1,27 @@
-<script>
-	import IconCodePen from '../icons/IconCodePen.svelte';
-	import IconGithub from '../icons/IconGithub.svelte';
-	import IconLinkedIn from '../icons/IconLinkedIn.svelte';
-	import IconStackOverflow from '../icons/IconStackOverflow.svelte';
-</script>
-
 <a href="https://www.github.com/quangdaon">
-	<IconGithub />
+	<iconify-icon icon="fa6-brands:github" width="100%"></iconify-icon>
 </a>
+
+<a href="https://hachyderm.io/@quangdao">
+	<iconify-icon icon="fa6-brands:mastodon" width="100%"></iconify-icon>
+</a>
+
 <a href="https://www.linkedin.com/in/quangdaonguyen/">
-	<IconLinkedIn />
+	<iconify-icon icon="fa6-brands:linkedin-in" width="100%"></iconify-icon>
 </a>
 
 <a href="https://codepen.io/quangdaon">
-	<IconCodePen />
+	<iconify-icon icon="fa6-brands:codepen" width="100%"></iconify-icon>
 </a>
 
 <a href="https://stackoverflow.com/users/6425704/quangdao-nguyen">
-	<IconStackOverflow />
+	<iconify-icon icon="fa6-brands:stack-overflow" width="100%"></iconify-icon>
 </a>
+
+
+<style>
+	iconify-icon {
+		color: var(--color-foreground);
+		width: 1em;
+	}
+</style>
