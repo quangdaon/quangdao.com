28ca365015175fdce5e295a03f14e45ebab30cc1
Quangdao Nguyen <qnguyen@nvisia.com>
2025-04-09 16:29:17 -0500

    More netlfiy?

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			split: true
+		}),
 		alias: {
 			'~': './src/styles/utils',
 			'@brand': './node_modules/@quangdao/brandkit/scss',
