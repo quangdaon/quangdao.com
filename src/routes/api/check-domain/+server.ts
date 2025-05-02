c6c473761ff3cf0a2ef7bd1f62b7853a97bb037c
Quangdao Nguyen <me@quangdao.com>
2025-05-02 15:49:52 -0500

    goof

---
+++ src/routes/api/check-domain/+server.ts
@@ -0,0 +1,46 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async ({ url, request }) => {
+	const origin = request.headers.get('origin') ?? '';
+
+	const domain = url.searchParams.get('domain');
+	const headers = {
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': origin.includes('quangdao.com') ? origin : 'not_allowed'
+	};
+
+	if (!domain) {
+		return new Response(JSON.stringify({ error: 'Missing domain parameter' }), {
+			status: 400,
+			headers
+		});
+	}
+
+	const requestUrl = `http://${domain}`;
+
+	try {
+		const res = await fetch(requestUrl);
+
+		return new Response(
+			JSON.stringify({
+				statusCode: res.status,
+				state: !res.ok ? 'error' : 'active'
+			}),
+			{
+				status: 200,
+				headers
+			}
+		);
+	} catch {
+		return new Response(
+			JSON.stringify({
+				statusCode: null,
+				state: 'inactive'
+			}),
+			{
+				status: 200,
+				headers
+			}
+		);
+	}
+};
