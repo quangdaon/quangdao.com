569fd2df978de6d52c64ca36f40b7a0988238438
Quangdao Nguyen <me@quangdao.com>
2026-01-31 23:16:33 -0600

    Sovereign hands post (#52)
    * Audit fix
    
    * Blog post

--- src/lib/components/shared/SocialIcons.svelte
+++ src/lib/components/shared/SocialIcons.svelte
@@ -6,16 +6,12 @@
 	<iconify-icon icon="fa6-brands:mastodon" width="100%"></iconify-icon>
 </a>
 
+<a href="https://bsky.app/profile/quangdao.bsky.social" aria-label="Bluesky">
+	<iconify-icon icon="fa6-brands:bluesky" width="100%"></iconify-icon>
+</a>
+
 <a href="https://www.linkedin.com/in/quangdaonguyen/" aria-label="LinkedIn">
 	<iconify-icon icon="fa6-brands:linkedin-in" width="100%"></iconify-icon>
-</a>
-
-<a href="https://codepen.io/quangdaon" aria-label="Codepen">
-	<iconify-icon icon="fa6-brands:codepen" width="100%"></iconify-icon>
-</a>
-
-<a href="https://stackoverflow.com/users/6425704/quangdao-nguyen" aria-label="Stack Overflow">
-	<iconify-icon icon="fa6-brands:stack-overflow" width="100%"></iconify-icon>
 </a>
 
 
